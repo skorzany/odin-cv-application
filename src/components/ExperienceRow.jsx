64857a5ba63091b976b06af7610b45aae2ef80d2
skorzany <177423232+skorzany@users.noreply.github.com>
2026-02-12 19:22:32 +0100

    Style the ExperienceRow component

--- src/components/ExperienceRow.jsx
+++ src/components/ExperienceRow.jsx
@@ -2,34 +2,71 @@ import styles from '../assets/styles/ExperienceRow.module.css';
 
 export default function ExperienceRow() {
   return (
-    <>
+    <div className={styles.row}>
       <>
-        {'*'}
-        <input type="date" name="workStart" min={past} max={today} required />
-        {' - '}
-        <input type="date" name="workEnd" min={past} max={today} />
+        <p className={styles.workPeriod}>01.01.2005 - 01.01.2007</p>
+        <p className={styles.company}>
+          Company name - <em>Position name</em>
+        </p>
+        <p className={styles.details}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+          eos fugit odio adipisci ad quasi distinctio in id nobis ipsam
+          perspiciatis, voluptates cumque debitis? Optio ipsa reiciendis
+          assumenda sed reprehenderit.
+        </p>
+        <button className={styles.saveEdit} type="button">
+          Save
+        </button>
+      </>
+      <>
+        <div className={styles.dates}>
+          <span className={styles.dateStart}>
+            {'*'}
+            <input
+              type="date"
+              className={styles.dateInput}
+              name="workStart"
+              min={past}
+              max={today}
+              required
+            />
+          </span>
+          <span className={styles.dateEnd}>
+            {'-'}
+            <input
+              type="date"
+              className={styles.dateInput}
+              name="workEnd"
+              min={past}
+              max={today}
+            />
+          </span>
+        </div>
         <input
           type="text"
+          className={styles.textInput}
           name="company"
           placeholder="Company (required)"
           required
         />
         <input
           type="text"
+          className={styles.textInput}
           name="position"
           placeholder="Position (required)"
           required
         />
-        <textarea name="details" rows="3" placeholder="Work details"></textarea>
+        <textarea
+          className={styles.textArea}
+          name="details"
+          rows="2"
+          placeholder="Work details"
+        ></textarea>
+        <button className={styles.saveEdit} type="button">
+          Save
+        </button>
       </>
-      <p>
-        <span>2005 - 2007</span>
-        <strong>Company name</strong>
-        <strong>Position name</strong>
-        <span>Lorem ipsum dolor sit amet...</span>
-      </p>
-      <button type="button">Save</button>
-    </>
+    </div>
   );
 }
 
