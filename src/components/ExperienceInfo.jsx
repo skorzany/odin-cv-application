2bfc47d1bd491a0dbc49afd60241bcf03a6d5983
skorzany <177423232+skorzany@users.noreply.github.com>
2026-02-19 18:03:19 +0100

    Connect ExperienceRow with ExperienceInfo

--- src/components/ExperienceInfo.jsx
+++ src/components/ExperienceInfo.jsx
@@ -1,19 +1,84 @@
+import { useState } from 'react';
 import ExperienceRow from './ExperienceRow.jsx';
 import styles from '../assets/styles/ExperienceInfo.module.css';
 
+const TEMPLATE = {
+  startDate: '',
+  endDate: '',
+  company: '',
+  position: '',
+  details: '',
+};
+let freeId = 0;
+
 export default function ExperienceInfo() {
+  const [jobs, setJobs] = useState([]);
+
+  function addJob() {
+    const newJob = { ...TEMPLATE, id: freeId++ };
+    setJobs([...jobs, newJob]);
+  }
+
+  function handleChange(e, id) {
+    const oldData = jobs.find((job) => job.id === id);
+    const idx = jobs.indexOf(oldData);
+    const name = e.target.name;
+    const value = e.target.value;
+    let newData;
+    switch (name) {
+      case 'workStart':
+        newData = { ...oldData, startDate: value };
+        break;
+      case 'workEnd':
+        newData = { ...oldData, endDate: value };
+        break;
+      case 'company':
+        newData = { ...oldData, company: value };
+        break;
+      case 'position':
+        newData = { ...oldData, position: value };
+        break;
+      case 'details':
+        newData = { ...oldData, details: value };
+        break;
+    }
+    setJobs([...jobs.slice(0, idx), newData, ...jobs.slice(idx + 1)]);
+  }
+
+  function handleSave(id) {
+    const oldData = jobs.find((job) => job.id === id);
+    const idx = jobs.indexOf(oldData);
+    const newData = {
+      ...oldData,
+      company: oldData.company.trim(),
+      position: oldData.position.trim(),
+      details: oldData.details.trim(),
+    };
+    setJobs([...jobs.slice(0, idx), newData, ...jobs.slice(idx + 1)]);
+  }
+
   return (
     <fieldset>
       <legend>Work experience</legend>
       <ol className={styles.list}>
-        <li className={styles.listItem}>
-          <ExperienceRow />
-          <button className={styles.remove} type="button">
-            Remove
-          </button>
-        </li>
+        {jobs.map((job) => (
+          <li className={styles.listItem} key={job.id}>
+            <ExperienceRow
+              expData={job}
+              onChange={(e) => handleChange(e, job.id)}
+              onSave={() => handleSave(job.id)}
+            />
+            <button
+              className={styles.remove}
+              type="button"
+              onClick={() => setJobs(jobs.filter((obj) => obj.id !== job.id))}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
       </ol>
-      <button className={styles.add} type="button">
+      <button className={styles.add} type="button" onClick={addJob}>
         Add new work experience
       </button>
     </fieldset>
--- src/components/ExperienceRow.jsx
+++ src/components/ExperienceRow.jsx
@@ -1,71 +1,165 @@
+import { useState, useRef } from 'react';
 import styles from '../assets/styles/ExperienceRow.module.css';
 
-export default function ExperienceRow() {
+export default function ExperienceRow({ expData, onChange, onSave }) {
+  const [isViewing, setIsViewing] = useState(false);
+  const rowRef = useRef(null);
+
+  function validate() {
+    const [startDate, endDate] = rowRef.current.querySelectorAll(
+      `.${styles.dateInput}`,
+    );
+    const [company, position] = rowRef.current.querySelectorAll(
+      `.${styles.textInput}`,
+    );
+    if (startDate.value === '') {
+      startDate.setCustomValidity('Please enter the start date.');
+      startDate.reportValidity();
+      return false;
+    } else if (endDate.value !== '' && endDate.value < startDate.value) {
+      endDate.setCustomValidity(
+        'End date cannot be smaller than the start date.',
+      );
+      endDate.reportValidity();
+      return false;
+    } else if (company.value.trim() === '') {
+      company.setCustomValidity("Please enter the company's name.");
+      company.reportValidity();
+      return false;
+    } else if (position.value.trim() === '') {
+      position.setCustomValidity("Please enter the job's title.");
+      position.reportValidity();
+      return false;
+    }
+    return true;
+  }
+
+  function handleClick() {
+    if (isViewing) setIsViewing(!isViewing);
+    else {
+      const allValid = validate();
+      if (allValid === true) {
+        onSave();
+        setIsViewing(!isViewing);
+      }
+    }
+  }
+
+  function handleKeyDown(e) {
+    e.target.setCustomValidity('');
+    e.target.reportValidity();
+  }
+
+  function setMinEndDate(e) {
+    const endDateElement = rowRef.current.querySelector(
+      'input[name="workEnd"]',
+    );
+    endDateElement.min = e.target.value;
+  }
+
+  function formatDate(dateString) {
+    const MONTHS = [
+      // abbreviations according to the Yale University Library
+      'Jan.',
+      'Feb.',
+      'Mar.',
+      'Apr.',
+      'May',
+      'June',
+      'July',
+      'Aug.',
+      'Sept.',
+      'Oct.',
+      'Nov.',
+      'Dec.',
+    ];
+    const dateParts = dateString.split('-');
+    return `${MONTHS[Number(dateParts[1]) - 1]} ${dateParts[0]}`;
+  }
+
   return (
-    <div className={styles.row}>
-      <>
-        <p className={styles.workPeriod}>01.01.2005 - 01.01.2007</p>
-        <p className={styles.company}>
-          Company name - <em>Position name</em>
-        </p>
-        <p className={styles.details}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
-          eos fugit odio adipisci ad quasi distinctio in id nobis ipsam
-          perspiciatis, voluptates cumque debitis? Optio ipsa reiciendis
-          assumenda sed reprehenderit.
-        </p>
-        <button className={styles.saveEdit} type="button">
-          Save
-        </button>
-      </>
-      <>
-        <div className={styles.dates}>
-          <span className={styles.dateStart}>
-            {'*'}
-            <input
-              type="date"
-              className={styles.dateInput}
-              name="workStart"
-              min={past}
-              max={today}
-              required
-            />
-          </span>
-          <span className={styles.dateEnd}>
-            {'-'}
-            <input
-              type="date"
-              className={styles.dateInput}
-              name="workEnd"
-              min={past}
-              max={today}
-            />
-          </span>
-        </div>
-        <input
-          type="text"
-          className={styles.textInput}
-          name="company"
-          placeholder="Company (required)"
-          required
-        />
-        <input
-          type="text"
-          className={styles.textInput}
-          name="position"
-          placeholder="Position (required)"
-          required
-        />
-        <textarea
-          className={styles.textArea}
-          name="details"
-          rows="2"
-          placeholder="Work details"
-        ></textarea>
-        <button className={styles.saveEdit} type="button">
-          Save
-        </button>
-      </>
+    <div className={styles.row} ref={rowRef}>
+      {isViewing ? (
+        <>
+          <p className={styles.workPeriod}>
+            {`${formatDate(expData.startDate)} - `}
+            {expData.endDate !== '' ? (
+              formatDate(expData.endDate)
+            ) : (
+              <em>present</em>
+            )}
+          </p>
+          <p className={styles.company}>
+            {`${expData.company} - `}
+            <em>{expData.position}</em>
+          </p>
+          <p className={styles.details}>{expData.details}</p>
+        </>
+      ) : (
+        <>
+          <div className={styles.dates}>
+            <span className={styles.dateStart}>
+              {'*'}
+              <input
+                type="date"
+                className={styles.dateInput}
+                name="workStart"
+                value={expData.startDate}
+                min={past}
+                max={today}
+                required
+                onChange={(e) => {
+                  setMinEndDate(e);
+                  onChange(e);
+                }}
+              />
+            </span>
+            <span className={styles.dateEnd}>
+              {'-'}
+              <input
+                type="date"
+                className={styles.dateInput}
+                name="workEnd"
+                value={expData.endDate}
+                min={past}
+                max={today}
+                onChange={onChange}
+              />
+            </span>
+          </div>
+          <input
+            type="text"
+            className={styles.textInput}
+            name="company"
+            value={expData.company}
+            placeholder="Company (required)"
+            required
+            onKeyDown={handleKeyDown}
+            onChange={onChange}
+          />
+          <input
+            type="text"
+            className={styles.textInput}
+            name="position"
+            value={expData.position}
+            placeholder="Position (required)"
+            required
+            onKeyDown={handleKeyDown}
+            onChange={onChange}
+          />
+          <textarea
+            className={styles.textArea}
+            name="details"
+            value={expData.details}
+            rows="2"
+            placeholder="Work details (optional)"
+            onChange={onChange}
+          ></textarea>
+        </>
+      )}
+      <button className={styles.saveEdit} type="button" onClick={handleClick}>
+        {isViewing ? 'Edit' : 'Save'}
+      </button>
     </div>
   );
 }
